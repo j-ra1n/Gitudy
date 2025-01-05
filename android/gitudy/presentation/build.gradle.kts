c45857e7d17cf6701a183a2a6759bd002cab4bb7
탁세하 <takseha98@gmail.com>
2025-01-05 23:05:52 +0900

    [AN] fix(#655): material 라이브러리 업데이트 - 이전 버전에 일부 상태 복원 관련 버그가 보고된 바 있음

--- android/gitudy/presentation/build.gradle.kts
+++ android/gitudy/presentation/build.gradle.kts
@@ -47,30 +47,25 @@ dependencies {
     implementation(project(":data"))
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.annotation:annotation:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("com.google.firebase:firebase-messaging-ktx:23.4.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.annotation:annotation:1.9.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("com.google.firebase:firebase-messaging-ktx:24.1.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.core:core:1.13.1")
-    implementation("androidx.activity:activity:1.9.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-android:2.8.4")
+    implementation("androidx.activity:activity:1.9.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // androidx webkit
-    implementation("androidx.webkit:webkit:1.10.0")
+    implementation("androidx.webkit:webkit:1.12.1")
 
     // navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-
-    // safe args
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
 
     // security
     implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
@@ -88,8 +83,8 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     // viewModel
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    implementation("androidx.activity:activity-ktx:1.9.3")
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.12.0")
