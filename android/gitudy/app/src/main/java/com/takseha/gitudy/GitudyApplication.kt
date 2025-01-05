a01e61217903ddc02488aefca561170380211f4b
탁세하 <takseha98@gmail.com>
2025-01-05 22:41:57 +0900

    [AN] fix(#655): 시스템 다크모드 시에도 라이트모드 앱 UI 나타나도록 수정

--- android/gitudy/app/src/main/java/com/takseha/gitudy/GitudyApplication.kt
+++ android/gitudy/app/src/main/java/com/takseha/gitudy/GitudyApplication.kt
@@ -1,11 +1,13 @@
 package com.takseha.gitudy
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.takseha.data.api.gitudy.RetrofitInstance
 
 class GitudyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         RetrofitInstance.init(this)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
